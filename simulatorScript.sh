fe4051bb1dbd805b0105b3ad572366714532c94a
Nick <nicolai.trandafil@ellation.com>
2018-10-15 16:35:53 +0300

    Add colors for commands

--- simulatorScript.sh
+++ simulatorScript.sh
@@ -1,27 +1,31 @@
 
 #!/usr/bin/env bash
 
+CYAN='\033[1;36m'
+GREEN='\033[0;32m'
+WHITE='\033[1;37m'
+
 #show available iOS runtimes
 runtimes=$(node -pe "JSON.parse(process.argv[1]).runtimes.map(x => x.name)" "$(xcrun simctl list runtimes -j)")
-echo "Available iOS versions:\n $runtimes \n"
+echo "${GREEN}Available iOS versions:\n $runtimes \n"
 
 #get user input of iosVersion
 defaultiOS='iOS 10.2'
-echo "Enter iOS version, default is $defaultiOS - "
+echo "${CYAN}Enter iOS version, default is $defaultiOS - ${WHITE}"
 read version
 iosVersion=${version:-$defaultiOS}
-echo "Selected version - $iosVersion \n\n"
+echo "${GREEN}Selected version - $iosVersion \n\n"
 
 #show available device types
 deviceTypes=$(node -pe "JSON.parse(process.argv[1]).devices['$iosVersion'].map(x => x.name)" "$(xcrun simctl list devices -j)")
-echo "Available device types:\n $deviceTypes \n"
+echo "${GREEN}Available device types:\n $deviceTypes \n"
 
 #get user input of deviceType
 defaultDevice='iPhone 6s'
-echo "Enter device, default is $defaultDevice - "
+echo "${CYAN}Enter device, default is $defaultDevice - ${WHITE}"
 read deviceType
 device=${deviceType:-$defaultDevice}
-echo "Selected device - $device \n\n"
+echo "${GREEN}Selected device - $device \n\n"
 
 #Getting udid for specific ios device
 udid=$(node -pe "JSON.parse(process.argv[1]).devices['$iosVersion'].find(x => x.name === '$device').udid" "$(xcrun simctl list devices -j)")
@@ -29,15 +33,15 @@ udid=$(node -pe "JSON.parse(process.argv[1]).devices['$iosVersion'].find(x => x.
 xcrun simctl boot $udid
 
 defaultURL='http://localhost:3000'
-echo "Enter url to open in simulator, default - $defaultURL "
+echo "${CYAN}Enter url to open in simulator, default - $defaultURL ${WHITE}"
 read url
 finalURL=${url:-$defaultURL}
 xcrun simctl openurl $udid $finalURL
 
 #Show booted devices
-echo "Booted devices:\n "
+echo "${GREEN}Booted devices:\n "
 xcrun simctl list devices | egrep '(Booted)'
-echo "\n To shutdown devices run in terminal 'xcrun simctl shutdown all':\n "
+echo "${CYAN}\n To shutdown devices run in terminal ${GREEN}'xcrun simctl shutdown all'\n "
 
 
 
